60ccfda6bdaf81d4a2b2dea58a17bdf2c1b8b785
hyunsang-ahn <happylovetkd@naver.com>
2022-07-12 06:55:21 +0900

    befor eject

---
+++ src/components/Red.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import './Red.css'
+
+const Red = () => {
+  console.log('red ....')
+
+  return <div className='Red'>Red</div>
+}
+
+export default Red
---
+++ src/pages/RedPage.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import Red from '../components/Red';
+
+const RedPage = () => {
+  console.log('red page....')
+  return <Red />
+}
+
+export default RedPage
